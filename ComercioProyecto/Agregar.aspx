06f44ed00b6e9bae985927a2a29fa9577fd07a49
VARELA BLANCO <jonathan.varela@ulatina.net>
2023-12-04 23:18:04 -1200

    Agregar archivos de proyecto.

---
+++ ComercioProyecto/Agregar.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Agregar.aspx.cs" Inherits="ComercioProyecto.Agregar" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Proyecto Comercio Electronico<br />
+            <br />
+            Nombre&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server" Height="16px" style="margin-bottom: 0px" Width="171px"></asp:TextBox>
+            <br />
+            <br />
+            Cedula&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server" Height="16px" Width="173px"></asp:TextBox>
+            <br />
+            <br />
+            Direccion&nbsp;
+            <asp:TextBox ID="TextBox3" runat="server" Width="171px"></asp:TextBox>
+            <br />
+            <br />
+            Fecha&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox4" runat="server" Width="171px"></asp:TextBox>
+            <br />
+            <br />
+            Total de Pago&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox5" runat="server" Width="136px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="BtnAgregar" runat="server" OnClick="BtnAgregar_Click" Text="Agregar" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnEditar" runat="server" Text="Editar" OnClick="btnEditar_Click" />
+&nbsp;&nbsp;
+            <asp:Button ID="btnBuscar" runat="server" Text="Buscar" OnClick="btnBuscar_Click" />
+            &nbsp;&nbsp;
+            <asp:Button ID="btnBorrar" runat="server" OnClick="btnBorrar_Click" Text="Borrar" />
+&nbsp;&nbsp;
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ComercioProyecto/Agregar.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace ComercioProyecto
+{
+    public partial class Agregar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            string s =
+                System.Configuration.ConfigurationManager.ConnectionStrings["cadenaconexion1"].ConnectionString;
+            SqlConnection conexion = new SqlConnection(s);
+            conexion.Open();
+            SqlCommand comando = new SqlCommand("insert into CLIENTE (NOMBRE, CEDULA, DIRECCION, FECHA, TOTALPAGO) values('" +
+                TextBox1.Text + "','" + this.TextBox2.Text + "','" + this.TextBox3.Text + "','" + this.TextBox4.Text + "','" + TextBox5.Text + "' )", conexion);
+            comando.ExecuteNonQuery();
+            Label1.Text = "Se agrego correctamente";
+            conexion.Close();
+        }
+
+        protected void btnBorrar_Click(object sender, EventArgs e)
+        {
+            string s =
+            System.Configuration.ConfigurationManager.ConnectionStrings["cadenaconexion1"].ConnectionString;
+            SqlConnection conexion = new SqlConnection(s);
+            conexion.Open();
+            SqlCommand comando = new SqlCommand("delete from CLIENTE where CEDULA='" +
+                this.TextBox2.Text + "'", conexion);
+            comando.ExecuteNonQuery();
+            Label1.Text = "Se elimino correctamente";
+            conexion.Close();
+        }
+
+        protected void btnEditar_Click(object sender, EventArgs e)
+        {
+            string s =
+            System.Configuration.ConfigurationManager.ConnectionStrings["cadenaconexion1"].ConnectionString;
+            SqlConnection conexion = new SqlConnection(s);
+            conexion.Open();
+            SqlCommand comando = new SqlCommand("update CLIENTE set NOMBRE='" +
+                this.TextBox1.Text + "', DIRECCION='" + this.TextBox3.Text + "', FECHA='" + this.TextBox4.Text + "', TOTALPAGO='" + this.TextBox5.Text + "' where CEDULA='" +
+                this.TextBox2.Text + "'", conexion);
+            comando.ExecuteNonQuery();
+            Label1.Text = "Se edito correctamente";
+            conexion.Close();
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string s = System.Configuration.ConfigurationManager.ConnectionStrings["cadenaconexion1"].ConnectionString;
+            using (SqlConnection conexion = new SqlConnection(s))
+            {
+                conexion.Open();
+                string nombre = TextBox1.Text;
+                string cedula = this.TextBox2.Text;
+                string direccion = this.TextBox3.Text;
+                string fecha = this.TextBox4.Text;
+                string totalPago = TextBox5.Text;
+
+                SqlCommand comando = new SqlCommand("select * from CLIENTE where NOMBRE = @nombre and CEDULA = @cedula and DIRECCION = @direccion and FECHA = @fecha and TOTALPAGO = @totalPago", conexion);
+                comando.Parameters.AddWithValue("@nombre", nombre);
+                comando.Parameters.AddWithValue("@cedula", cedula);
+                comando.Parameters.AddWithValue("@direccion", direccion);
+                comando.Parameters.AddWithValue("@fecha", fecha);
+                comando.Parameters.AddWithValue("@totalPago", totalPago);
+
+                SqlDataReader resultado = comando.ExecuteReader();
+
+                if (resultado.Read())
+                {
+                    Label1.Text = "El cliente si existe en la base de datos";
+                    TextBox1.Text = resultado["NOMBRE"].ToString();
+                    this.TextBox2.Text = resultado["CEDULA"].ToString();
+                    this.TextBox3.Text = resultado["DIRECCION"].ToString();
+                    this.TextBox4.Text = resultado["FECHA"].ToString();
+                    TextBox5.Text = resultado["TOTALPAGO"].ToString();
+                }
+                else
+                {
+                    Label1.Text = "El cliente no existe en la base de datos";
+                }
+
+                resultado.Close();
+                conexion.Close();
+            }
+
+
+        }
+    }
+
+}
+
+
+
